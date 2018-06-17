17f452a67cb82b1d9b1617741411ac19cc18afc3
mrl5 <jakuboo+github@gmail.com>
2018-06-17 18:27:05 +0200

    comments, formatting

--- RawCodeProvider.pm
+++ RawCodeProvider.pm
@@ -8,6 +8,7 @@
 # References:
 # 1) https://stackoverflow.com/questions/1712016/how-do-i-include-functions-from-another-file-in-my-perl-script
 
+#perlmod
 package RawCodeProvider;
 use strict;
 use warnings;
@@ -17,26 +18,25 @@ my @lines;
 my @rawSourceCode;
 my $multiLineComment = 0;
 
-our @ISA= qw( Exporter );
-our @EXPORT = qw( getRawCode );
+#perlmod
+our @ISA = qw(Exporter);
+our @EXPORT = qw(getRawCode);
 
 sub getRawCode {
+    #read all lines
     while (<>) {
         #remove \n from end of the line
         chomp;
-
         #remove spaces from the beggining of the line
         s/^\s+//;
         #remove '/* comments */'
         s/^\/\*.*\*\///;
         #remove '//comments'
         s/^\/\/.*//;
-
         #add non-empty line to the end of array
         if ($_ ne '') {
             push(@lines, $_);
         }
-
     }
 
     foreach (@lines) {
@@ -55,6 +55,5 @@ sub getRawCode {
             push(@rawSourceCode, $_);
         }
     }
-
     return @rawSourceCode;
 }
