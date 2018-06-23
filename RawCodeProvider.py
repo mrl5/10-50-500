f79f93f871aab945896ee072e0bdf97c1d87905d
mrl5 <jakuboo+github@gmail.com>
2018-06-23 15:26:08 +0200

    get raw java code

--- RawCodeProvider.py
+++ RawCodeProvider.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 
+import re
+
+
+def getRawCode(filePath):
+    with open(filePath, "r") as f:
+        rawCode = []
+        multiLineComment = False
+
+        for line in f:
+            line = re.sub(r'^\s+', '', line).rstrip()
+            line = re.sub(r'^\/\*.*\*\/', '', line).rstrip()
+
+            if line != '':
+                if (re.match(r'^\/(\*)+', line)):
+                    multiLineComment = True
+                    continue
+                elif (re.match(r'(\*)+\/', line)):
+                    multiLineComment = False
+                    continue
+                elif (multiLineComment == False):
+                    rawCode.append(line)
+    return rawCode
