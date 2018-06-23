33d2e5bbfc8cf72bf4ca26d8510088aa0d828af0
mrl5 <jakuboo+github@gmail.com>
2018-06-23 15:51:17 +0200

    exclude "//comments"

--- RawCodeProvider.py
+++ RawCodeProvider.py
@@ -9,16 +9,26 @@ def getRawCode(filePath):
         multiLineComment = False
 
         for line in f:
+            # remove spaces from the beggining of the line
             line = re.sub(r'^\s+', '', line).rstrip()
+            # remove '/* comments */'
             line = re.sub(r'^\/\*.*\*\/', '', line).rstrip()
+            # remove '//comments'
+            line = re.sub(r'^\/\/.*', '', line).rstrip()
 
+            # ignore empty lines
             if line != '':
+                # skip /** multi-line comments
                 if (re.match(r'^\/(\*)+', line)):
                     multiLineComment = True
                     continue
+
+                # check if multi-line comment was closed
                 elif (re.match(r'(\*)+\/', line)):
                     multiLineComment = False
                     continue
+
+                # add line if it's not multi-line comment
                 elif (multiLineComment == False):
                     rawCode.append(line)
     return rawCode
