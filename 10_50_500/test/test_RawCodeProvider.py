6c6ba0b21ce7e892097031270996850b22e6fbf5
mrl5 <jakuboo+github@gmail.com>
2018-09-28 22:14:52 +0200

    test case: remove trailing newlines

--- 10_50_500/test/test_RawCodeProvider.py
+++ 10_50_500/test/test_RawCodeProvider.py
@@ -8,23 +8,30 @@ __author__ = "mrl5"
 
 """
 Scenario:
-A. get_raw_code
-    1. Remove spaces from the beginning of the line and remove trailing newline
-    2. Remove '/* comments */'
-    3. Remove '//comments'
-    4. Remove trailing whitespaces
-    5. Remove multi-line comments (/*\n\n\n*/)
-    6. Read from file
-    7. Return list with raw code
+1. get_raw_code
+    - remove spaces from the beginning of the line
+    - remove trailing newline
+    - remove '/* comments */'
+    - remove '//comments'
+    - remove trailing whitespaces
+    - remove multi-line comments (/*\n\n\n*/)
+    - read from file
+    - return list with raw code
     
-B. pretty_print
-    1. "something {"
-    2. "}" or "} else"
-    3. "} catch (NullPointerException e) {"
+2. pretty_print
+    - "something {"
+    - "}" or "} else"
+    - "} catch (NullPointerException e) {"
 """
 
 
 def test_rm_leadnig_spaces():
     input_string = "    never ending story"
-    return_string = "never ending story"
-    assert get_raw_code(input_string) == return_string
+    output_string = "never ending story"
+    assert get_raw_code(input_string) == output_string
+
+
+def test_rm_trailing_newline():
+    input_string = "this is how we do it\n\n\n"
+    output_string = "this is how we do it"
+    assert get_raw_code(input_string) == output_string
