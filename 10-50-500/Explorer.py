97b5c4cfb8e5e2f350057b314886562183c5e84f
mrl5 <jakuboo+github@gmail.com>
2018-09-28 12:39:42 +0200

    unit tests: self.get_project_structure()

--- 10-50-500/Explorer.py
+++ 10-50-500/Explorer.py
@@ -9,6 +9,12 @@ class Explorer:
     Class for Maven project exploration
     """
 
+    class NotAMavenStandardDirectoryLayoutError(Exception):
+        """
+        Custom exception
+        """
+        pass
+
     def __init__(self, project_dir=os.getcwd()):
         self.project_dir = project_dir
         self._packages = {}
@@ -17,18 +23,40 @@ class Explorer:
         """
         Adds package and it's classes to the self._packages dictionary
         :param path: relative path to package (e.g. com/tuxnet/package)
+        :raises FileNotFoundError: when a directory is requested but doesn't exist
+        :raises NotADirectoryError: when path leads to something which is not a directory
         """
         package_classes = {}
         for item in os.listdir(path):
-            path_to_item = os.path.join(path, item)
+            path_to_item = os.path.join(os.path.abspath(path), item)
             if os.path.isfile(path_to_item):
                 if os.path.basename(path_to_item).endswith(".java"):
                     java_class = {os.path.splitext(os.path.basename(path_to_item))[0]: path_to_item}
                     package_classes.update(java_class)
-        # uptade self._packages only if package has .java files
+        # update self._packages only if package has .java files
         if bool(package_classes):
             package = {get_package_name(path): package_classes}
             self._packages.update(package)
+
+    def get_project_structure(self):
+        if verify_directory_layout(self.project_dir):
+            java_sources = os.path.join(self.project_dir, "src", "main", "java")
+            # add packages to the set
+            packages = set()
+            for root, dirs, files in os.walk(java_sources, topdown=False):
+                for file in files:
+                    package = re.sub("{}{}".format("^", java_sources), '', root)
+                    # //com/some/package -> com/some/package
+                    package = re.sub("{}{}{}".format("^", os.sep, "+"), '', package)
+                    packages.add(package)
+            os.chdir(java_sources)
+            # create dictionary of project
+            for package in packages:
+                self._add_package(package)
+        else:
+            raise self.NotAMavenStandardDirectoryLayoutError(
+                "{} directory doesn't have Maven's Standard Directory Layout (src/main/java)".format(self.project_dir))
+        return self._packages
 
 
 def verify_directory_layout(project_dir):
@@ -40,6 +68,7 @@ def verify_directory_layout(project_dir):
     :raises NotADirectoryError: when path leads to something which is not a directory
     :return: True if directory from path has Maven's "Standard Directory Layout"; else False
     """
+    # throws exceptions if directory doesn't exist
     os.chdir(project_dir)
     maven_standard_directory_layout = os.path.join("src", "main", "java")
     path_to_analyse = os.path.join(project_dir, maven_standard_directory_layout)
--- 10-50-500/test/test_Explorer.py
+++ 10-50-500/test/test_Explorer.py
@@ -21,16 +21,15 @@ Scenario:
     - /com/company/package -> com.company.package
     - //com/company//package -> com.company.package
     
-4) test 'add_package' method:
+4) test '_add_package' method:
     - method should add new item to the dictionary (named as package) with all filenames and full paths to the .java files
     - only .java files from the first level should be added
     - don't create dictionary for empty package
 
 5) test 'get_project_structure' method:
-    - if project doesn't have "Standard Directory Layout": throw "NotAStandardDirectoryLayout" exception
-    - on "FileNotFoundError" exception: print error
-    - on "NotADirectoryError" exception: print error
-    - if "Standard Directory Layout": fill 'self.packages' dictionary with structure "{package: {class: path/to/class}}"
+    - if project doesn't have "Standard Directory Layout": throw "NotAMavenStandardDirectoryLayoutError" exception
+    - don't handle 'verify_directory_layout' exceptions
+    - if "Standard Directory Layout": retunr 'self._packages' dictionary with structure "{package: {class: path/to/class}}"
 """
 
 
@@ -40,11 +39,16 @@ def explorer():
     return explorer
 
 
-# verify_directory_layout
-def test_correct_directory_layout(tmpdir):
+@pytest.fixture()
+def maven_sdl(tmpdir):
     test_project = tmpdir.mkdir("test_project")
     os.makedirs(os.path.join(str(test_project), "src", "main", "java"))
-    assert verify_directory_layout(str(test_project)) is True
+    return test_project
+
+
+# verify_directory_layout
+def test_correct_directory_layout(maven_sdl):
+    assert verify_directory_layout(str(maven_sdl)) is True
 
 
 def test_false_directory_layout(tmpdir):
@@ -55,7 +59,7 @@ def test_false_directory_layout(tmpdir):
 
 def test_path_doesnt_exist(tmpdir):
     test_dir = tmpdir.mkdir("test_dir")
-    imaginary_project =  os.path.join(str(test_dir), "imaginary", "project")
+    imaginary_project = os.path.join(str(test_dir), "imaginary", "project")
     with pytest.raises(FileNotFoundError):
         verify_directory_layout(imaginary_project)
 
@@ -63,7 +67,6 @@ def test_path_doesnt_exist(tmpdir):
 def test_path_to_file(tmpdir):
     imaginary_project = tmpdir.mkdir("test_dir").join("im_file.txt")
     imaginary_project.write("content")
-    print(str(imaginary_project))
     with pytest.raises(NotADirectoryError):
         verify_directory_layout(str(imaginary_project))
 
@@ -80,7 +83,7 @@ def test_multi_sep_package_name():
     assert get_package_name(path) == "com.tuxnet.package"
 
 
-# self.add_package
+# self._add_package
 def test_add_package(explorer, tmpdir):
     package_dir = tmpdir.mkdir("com").mkdir("package")
     some_dir = "some_dir"
@@ -88,6 +91,7 @@ def test_add_package(explorer, tmpdir):
     package_name = get_package_name(str(package_dir))
     java_classes = {}
     test_item = {package_name: java_classes}
+
     for java_class in '12345':
         java_file = str("{}.{}").format(java_class, "java")
         some_file = str("{}.{}").format(java_class, "txt")
@@ -96,6 +100,7 @@ def test_add_package(explorer, tmpdir):
         os.mknod(os.path.join(str(package_dir), some_dir, java_file))
         os.mknod(os.path.join(str(package_dir), some_dir, some_file))
         java_classes.update({java_class: os.path.join(str(package_dir), java_file)})
+
     explorer._add_package(str(package_dir))
     assert test_item[package_name] == explorer._packages[package_name]
 
@@ -103,8 +108,44 @@ def test_add_package(explorer, tmpdir):
 def test_empty_package(explorer, tmpdir):
     package_dir = tmpdir.mkdir("com").mkdir("package")
     package_name = get_package_name(str(package_dir))
-    java_classes = {}
-    test_item = {package_name: java_classes}
+
     explorer._add_package(str(package_dir))
     with pytest.raises(KeyError):
         print(explorer._packages[package_name])
+
+
+# self.get_project_structure
+def test_NotAMavenStandardDirectoryLayoutError_exception(explorer, tmpdir):
+    test_project = tmpdir.mkdir("test_project")
+    os.makedirs(os.path.join(str(test_project), "some", "random", "dir"))
+
+    explorer.project_dir = str(test_project)
+    with pytest.raises(explorer.NotAMavenStandardDirectoryLayoutError):
+        explorer.get_project_structure()
+
+
+def test_get_project_structure(explorer, maven_sdl):
+    java_sources = os.path.join(os.path.abspath(str(maven_sdl)), "src", "main", "java")
+    pkg_one = os.path.join(java_sources, "com", "tuxnet", "p_one")
+    pkg_two = os.path.join(pkg_one, "p_two")
+    pkg_three = os.path.join(java_sources, "com", "tuxnet", "p_three")
+    pkgs = ["com.tuxnet.p_one", "com.tuxnet.p_one.p_two", "com.tuxnet.p_three"]
+
+    packages = {pkg: [pkg_one, pkg_two, pkg_three][i] for i, pkg in enumerate(pkgs)}
+    synthetic_result = {pkgs[0]: {}, pkgs[1]: {}, pkgs[2]: {}}
+    # for each package create sample .java files
+    for package_name, package_path in packages.items():
+        # necessary if com/tuxnet/p_one/p_two was created before com/tuxnet/p_one
+        try:
+            os.makedirs(package_path)
+        except FileExistsError:
+            pass
+        package_classes = {}
+        for java_class in '12':
+            java_file = str("{}.{}").format(java_class, "java")
+            os.mknod(os.path.join(package_path, java_file))
+            package_classes.update({java_class: os.path.join(package_path, java_file)})
+        synthetic_result[package_name].update(package_classes)
+
+    explorer.project_dir = str(maven_sdl)
+    assert explorer.get_project_structure() == synthetic_result
