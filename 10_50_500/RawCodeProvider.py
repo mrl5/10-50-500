133962412dfde067cda4512982b66a1be63776f6
mrl5 <jakuboo+github@gmail.com>
2018-09-28 22:08:50 +0200

    test case: remove leading spaces

--- 10_50_500/RawCodeProvider.py
+++ 10_50_500/RawCodeProvider.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import re
 
 __author__ = "mrl5"
+
+
+def get_raw_code(input_string):
+    """
+    Removes empty lines, leading and trailing whitespaces, single and multi line comments
+
+    :param file_path: path to .java file
+    :return: list with raw code
+    """
+    line = re.sub(r'''
+        ^   # start of string
+        \s+ # one or more whitespaces
+        ''', '', input_string, 0, re.VERBOSE)
+    return line
--- 10_50_500/test/test_RawCodeProvider.py
+++ 10_50_500/test/test_RawCodeProvider.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+import os
+from RawCodeProvider import get_raw_code
 
 __author__ = "mrl5"
 
 """
 Scenario:
 A. get_raw_code
-    1. Handle exceptions from 'with open()'
-    2. Remove spaces from the beginning of the line and remove trailing newline
-    3. Remove '/* comments */'
-    4. Remove '//comments'
-    5. Remove trailing whitespaces
-    6. Remove multi-line comments (/*\n\n\n*/)
+    1. Remove spaces from the beginning of the line and remove trailing newline
+    2. Remove '/* comments */'
+    3. Remove '//comments'
+    4. Remove trailing whitespaces
+    5. Remove multi-line comments (/*\n\n\n*/)
+    6. Read from file
+    7. Return list with raw code
     
 B. pretty_print
     1. "something {"
@@ -19,3 +23,8 @@ B. pretty_print
     3. "} catch (NullPointerException e) {"
 """
 
+
+def test_rm_leadnig_spaces():
+    input_string = "    never ending story"
+    return_string = "never ending story"
+    assert get_raw_code(input_string) == return_string
