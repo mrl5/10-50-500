baa3e6631ba9dff3fd1d44f1c08829b68e1177d5
mrl5 <jakuboo+github@gmail.com>
2018-09-29 10:33:33 +0200

    branch init

---
+++ 10_50_500/PrettyPrinter.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "mrl5"
+
+
+def pretty_print(raw_code):
+    pass
---
+++ 10_50_500/test/test_PrettyPrinter.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+__author__ = "mrl5"
+
+from PrettyPrinter import pretty_print
+
+"""
+Scenario:
+    - test if param is list
+    - "something {"
+    - "}" or "} else"
+    - "} catch (NullPointerException e) {"
+    - compare with stripped .java file
+"""
