ca7ef1985a0611d82a5a1fac32ac849f9d75e5b2
mrl5 <jakuboo+github@gmail.com>
2018-09-29 21:36:00 +0200

    test case: ignore brackets inside single quotes

--- 10_50_500/test/test_PrettyPrinter.py
+++ 10_50_500/test/test_PrettyPrinter.py
@@ -260,6 +260,16 @@ def test_brackets_inside_double_quotes(pretty_printer, raw_java_code, tabbed_jav
     assert pretty_printer.format_code() == tabbed_java_code
 
 
+def test_brackets_inside_single_quotes(pretty_printer, raw_java_code, tabbed_java_code):
+    pretty_printer.indentation = "\t"
+    start = 0
+    stop = 20
+    raw_java_code = raw_java_code[start:stop]
+    tabbed_java_code = tabbed_java_code[start:stop]
+    pretty_printer.unformatted_code_list = raw_java_code
+    assert pretty_printer.format_code() == tabbed_java_code
+
+
 @pytest.mark.skip
 def test_fourspaced_indent(pretty_printer, raw_java_code, fourspaced_java_code):
     pretty_printer.indentation = "    "
