1122b1c48cab901640e15f2c89f78938d73823ad
mrl5 <jakuboo+github@gmail.com>
2018-09-30 18:13:30 +0200

    feature: extra indent on line breaks

--- 10_50_500/PrettyPrinter.py
+++ 10_50_500/PrettyPrinter.py
@@ -17,7 +17,8 @@ class PrettyPrinter:
             "nest": "({+|^case\s+.*?:|^default\s*.*?:)",    # "{", "case x:", "default:"
             "unnest": "(}+)",                               # "}"
             "indent_break": "(\\bbreak\\b\s*;)",            # "break;"
-            "line_break": ".*[{};:]$"                       # chars which create new blocks of code
+            "line_end": ".*[{};:]$",                        # chars which end lines
+            "line_break": ".*[+\-*/]$"                             # chars for breaking lines
         }
         self._brackets_nesting = 0
 
@@ -74,6 +75,20 @@ class PrettyPrinter:
         nest_lvl = self._brackets_nesting if line.endswith("}") and self._brackets_nesting != nest_lvl else nest_lvl
         return nest_lvl
 
+    def _get_extra_indent(self, line_no, broken_line):
+        """
+        Provides indentation for broken lines
+        :param line_no: number of line to be analysed
+        :param broken_line: True if line is broken
+        :return: extra indent
+        """
+        output = ""
+        if broken_line:
+            if self.unformatted_code_list[line_no].startswith(".") or \
+                    re.search(self._regex_patterns["line_break"], self.unformatted_code_list[line_no - 1]):
+                output = self.indentation
+        return output
+
     def format_code(self, unformatted_code_list=None):
         """
         Formats code into blocks with an indentation style
@@ -87,8 +102,8 @@ class PrettyPrinter:
         formatted_code = []
         nest_lvl = 0
         broken_line = False
-        for line in self.unformatted_code_list:
-            line_break = True if not re.search(self._regex_patterns["line_break"], line) else False
+        for i, line in enumerate(self.unformatted_code_list):
+            line_break = True if not re.search(self._regex_patterns["line_end"], line) else False
             indent_break = re.search(self._regex_patterns["indent_break"], re.sub("[\"\'].*?[\"\']", '', line))
 
             nest_lvl = self._get_nest_lvl(line, nest_lvl, line_break)
@@ -96,9 +111,8 @@ class PrettyPrinter:
                                                    re.sub("[\"\'].*?[\"\']", '', line)) or line_break else False
             indent = nest_lvl * self.indentation if not wait_for_next_line else (nest_lvl - 1) * self.indentation
 
-            # Line break by method: object.method1().method2()
-            #                                 .method3()
-            indent += self.indentation if line.startswith(".") and broken_line else ''
+            # extra indent when line was broken
+            indent += self._get_extra_indent(i, broken_line)
             formatted_line = indent + line
             formatted_code.append(formatted_line)
 
